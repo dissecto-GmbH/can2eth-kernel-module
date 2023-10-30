4c7a57eefa9b1209d976ce377c8f1d782f31e48a
Matthias Unterrainer <matthias.unterrainer@st.oth-regensburg.de>
2023-10-30 14:58:29 +0100

    added tx

--- canToEthMod.c
+++ canToEthMod.c
@@ -1,27 +1,71 @@
 #include "canToEthMod.h"
 
-struct net_device *ctem_dev;
+static int setup_sock_addr(struct sockaddr **addr, int port, u32 ip);
 
-static int setup_sock_addr(struct sockaddr** addr,int port);
-
-static int setup_sock_addr(struct sockaddr** addr,int port)
+static int setup_sock_addr(struct sockaddr **addr, int port, u32 ip)
 {
-    struct sockaddr_in *udp_addr = kmalloc(sizeof(struct sockaddr_in),GFP_KERNEL);
+    struct sockaddr_in *udp_addr = kmalloc(sizeof(struct sockaddr_in), GFP_KERNEL);
 
-    if (udp_addr == NULL) return -ENOMEM;
+    if (udp_addr == NULL)
+        return -ENOMEM;
 
     memset(udp_addr, 0, sizeof(struct sockaddr_in));
     udp_addr->sin_family = PF_INET;
     udp_addr->sin_port = htons(port);
-    udp_addr->sin_addr.s_addr = htonl(INADDR_ANY);
-    *addr = (struct sockaddr*)udp_addr;
+    udp_addr->sin_addr.s_addr = htonl(ip);
+    *addr = (struct sockaddr *)udp_addr;
 
     return 0;
 }
 
+static void ctem_dellink(struct net_device *dev, struct list_head *head)
+{
+    struct ctem_priv *priv = netdev_priv(dev);
+
+    printk(KERN_DEBUG "%s: dellink\n", MODULE_NAME);
+
+    ctem_teardown_udp(dev);
+    kfree(priv->stats);
+
+    dev->flags |= IFF_DORMANT;
+
+    unregister_netdevice_queue(dev, NULL);
+}
+
+static struct net_device_stats *ctem_get_stats(struct net_device *dev)
+{
+    struct rtnl_link_stats64 stats64;
+
+    printk(KERN_DEBUG "%s: Get Stats\n", MODULE_NAME);
+
+    memset(&stats64, 0, sizeof(stats64));
+
+    ctem_get_stats64(dev, &stats64);
+
+    dev->stats.rx_bytes = stats64.rx_bytes;
+    dev->stats.rx_dropped = stats64.rx_dropped;
+    dev->stats.rx_errors = stats64.rx_errors;
+
+    dev->stats.tx_bytes = stats64.tx_bytes;
+    dev->stats.tx_dropped = stats64.tx_dropped;
+    dev->stats.tx_errors = stats64.tx_errors;
+
+    return &dev->stats;
+}
+
+static void ctem_get_stats64(struct net_device *dev, struct rtnl_link_stats64 *storage)
+{
+    struct ctem_priv *priv = netdev_priv(dev);
+
+    printk(KERN_DEBUG "%s: Get Stats64\n", MODULE_NAME);
+
+    memcpy(priv->stats, storage, sizeof(struct rtnl_link_stats64));
+}
+
 static int ctem_open(struct net_device *dev)
-{   
-    printk(KERN_INFO "%s: Opened %s\n",MODULE_NAME, dev->name);
+{
+    printk(KERN_INFO "%s: Opened %s\n", MODULE_NAME, dev->name);
+
     netif_carrier_on(dev);
     netif_start_queue(dev);
     return 0;
@@ -29,53 +73,162 @@ static int ctem_open(struct net_device *dev)
 
 static int ctem_stop(struct net_device *dev)
 {
-    printk(KERN_INFO "%s: Stopped %s\n",MODULE_NAME, dev->name);
+    printk(KERN_INFO "%s: Stopped %s\n", MODULE_NAME, dev->name);
+
     netif_carrier_off(dev);
     netif_stop_queue(dev);
     return 0;
 }
 
-static netdev_tx_t ctem_xmit(struct sk_buff* skb, struct net_device *dev )
+static netdev_tx_t ctem_xmit(struct sk_buff *skb, struct net_device *dev)
 {
+    struct ctem_priv *priv = netdev_priv(dev);
+    struct can_frame *frame = (struct can_frame *)skb->data;
+    struct msghdr msg;
+    struct kvec iov;
+    char can_frame_data[sizeof(struct can_frame)];
+    int ret;
+
+    if (printk_ratelimit())
+        printk(KERN_INFO "%s: TX with %d CAN - FRAME: Id: %d , %x %x %x %x %x %x %x %x\n", MODULE_NAME, frame->can_id, frame->data[0], frame->data[1], frame->data[2], frame->data[3], frame->data[4], frame->data[5], frame->data[6], frame->data[7]);
+
+    memset(&msg, 0, sizeof(msg));
+    memset(&iov, 0, sizeof(iov));
+
+    memcpy(can_frame_data, frame, sizeof(can_frame_data));
+
+    iov.iov_base = frame;
+    iov.iov_len = sizeof(can_frame_data);
+
+    msg.msg_name = priv->udp_addr_send;
+    msg.msg_namelen = sizeof(*priv->udp_addr_send);
+
+    ret = kernel_sendmsg(priv->udp_socket, &msg, &iov, 1, iov.iov_len);
+
+    if (ret < 0)
+    {
+        if (printk_ratelimit())
+            printk(KERN_WARNING "%s: Did not send message! Error: %d\n", MODULE_NAME, ret);
+
+        priv->stats->tx_errors++;
+        priv->stats->tx_dropped++;
+    }
+    else
+    {
+        if (printk_ratelimit())
+            printk(KERN_INFO "%s: Send %d bytes.\n", MODULE_NAME, ret);
+        priv->stats->tx_packets++;
+        priv->stats->tx_bytes += ret;
+    }
+
     dev_kfree_skb(skb);
     return NETDEV_TX_OK;
 }
 
+static void ctem_parse_frame(struct net_device *dev, unsigned char *buf, int size)
+{
+    struct can_frame *can_frame;
+    struct sk_buff *skb;
+    struct ctem_priv *priv = netdev_priv(dev);
+    int ret;
+
+    skb = netdev_alloc_skb(dev, sizeof(struct can_frame));
+
+    if (skb)
+    {
+        can_frame = (struct can_frame *)skb_put(skb, sizeof(struct can_frame));
+
+        can_frame->can_id = (buf[2] << 8) | buf[3];
+        can_frame->len = buf[4];
+        memcpy(can_frame->data, &buf[8], sizeof(CAN_MAX_DLEN));
+
+        skb->dev = dev;
+        skb->protocol = htons(ETH_P_CAN);
+        skb->ip_summed = CHECKSUM_UNNECESSARY;
+
+        ret = netif_rx(skb);
+
+        // update stats
+        if (ret == NET_RX_DROP)
+            priv->stats->rx_dropped++;
+        else if (ret == NET_RX_SUCCESS)
+        {
+            priv->stats->rx_bytes += can_frame->len;
+            priv->stats->rx_packets++;
+        }
+    }
+    else
+    {
+        priv->stats->rx_errors++;
+    }
+}
+
+static void ctem_test_parse_frame(struct net_device *dev)
+{
+    struct can_frame *cf;
+    struct sk_buff *can_skb;
+    int ans = 0x87654321;
+    int packet_len = sizeof(int);
+
+    int ans_net_byte_order = htonl(ans);
+
+    can_skb = dev_alloc_skb(packet_len + sizeof(struct can_frame));
+    if (can_skb)
+    {
+        cf = (struct can_frame *)skb_put(can_skb, sizeof(struct can_frame));
+        cf->len = packet_len;
+        cf->can_id = 0x321;
+        memcpy(cf->data, &ans_net_byte_order, packet_len);
+
+        // memcpy(skb_put(can_skb,packet_len),ans,packet_len);
+
+        can_skb->dev = dev;
+        can_skb->protocol = htons(ETH_P_CAN);
+        can_skb->ip_summed = CHECKSUM_UNNECESSARY;
+
+        netif_rx(can_skb);
+    }
+}
+
 static int ctem_packet_reception_thread(void *arg)
 {
-    struct net_device *dev = (struct net_device*) arg;
+    struct net_device *dev = (struct net_device *)arg;
     struct ctem_priv *priv = netdev_priv(dev);
 
-    printk(KERN_DEBUG "%s: Started listing\n", MODULE_NAME); 
+    printk(KERN_DEBUG "%s: Started listing\n", MODULE_NAME);
 
-    while(!kthread_should_stop())
+    while (!kthread_should_stop())
     {
         struct msghdr msg;
         struct sockaddr_in sender_addr;
         struct kvec iov;
-        struct can_frame can_frame;       
+        unsigned char receive_buffer[sizeof(struct can_frame)];
         int ret;
-        
+
         // initalize structs
         memset(&sender_addr, 0, sizeof(sender_addr));
         memset(&msg, 0, sizeof(msg));
         memset(&iov, 0, sizeof(iov));
-        memset(&can_frame, 0, sizeof(can_frame));
 
         // set up iov struct
-        iov.iov_base = &can_frame;
-        iov.iov_len = sizeof(struct can_frame);
+        iov.iov_base = receive_buffer;
+        iov.iov_len = sizeof(receive_buffer);
 
         ret = kernel_recvmsg(priv->udp_socket, &msg, &iov, 1, iov.iov_len, MSG_WAITALL);
         if (ret < 0)
         {
-            if(printk_ratelimit()) printk(KERN_DEBUG "%s: Error while listening to udp socket: %d\n", MODULE_NAME, ret);
+            if (printk_ratelimit())
+                printk(KERN_DEBUG "%s: Error while listening to udp socket: %d\n", MODULE_NAME, ret);
+
+            priv->stats->rx_errors++;
         }
         else
         {
-            if(printk_ratelimit()){                
-                printk(KERN_DEBUG "%s: Received %d Bytes\nCAN frame - ID: %x, DLC: %d, Data: %x %x %x %x %x %x %x %x\n", MODULE_NAME, ret, can_frame.can_id, can_frame.can_dlc, can_frame.data[0], can_frame.data[1], can_frame.data[2], can_frame.data[3], can_frame.data[4], can_frame.data[5], can_frame.data[6], can_frame.data[7]);     
-            }
+            // should propably use smth. like Work-Queues for this part
+            // ctem_parse_frame(dev, receive_buffer, sizeof(receive_buffer));
+
+            // just for testing
+            ctem_test_parse_frame(dev);
         }
     }
 
@@ -95,37 +248,40 @@ static int ctem_setup_udp(struct net_device *dev, int udp_listen_port, int udp_s
     int ret;
     struct ctem_priv *priv = netdev_priv(dev);
 
-    ret = setup_sock_addr(&priv->udp_addr_listen, udp_listen_port);
-    if(ret)
+    ret = setup_sock_addr(&priv->udp_addr_listen, udp_listen_port, INADDR_ANY);
+    if (ret)
     {
         printk(KERN_ERR "%s: Error setting up udp listen addr\n", MODULE_NAME);
         return ret;
     }
 
-    ret = setup_sock_addr(&priv->udp_addr_send, udp_send_port);
-    if(ret)
+    ret = setup_sock_addr(&priv->udp_addr_send, udp_send_port, in_aton("192.168.122.1"));
+    if (ret)
     {
         printk(KERN_ERR "%s: Error setting up udp send addr\n", MODULE_NAME);
         return ret;
     }
 
     // set up UDP socket
-    ret = sock_create_kern(&init_net, PF_INET, SOCK_DGRAM, IPPROTO_UDP,&(priv->udp_socket));
-    if (ret) {
+    ret = sock_create_kern(&init_net, PF_INET, SOCK_DGRAM, IPPROTO_UDP, &(priv->udp_socket));
+    if (ret)
+    {
         printk(KERN_ERR "%s: Error creating UDP socket: %d\n", MODULE_NAME, ret);
         return ret;
     }
 
     // bind UDP socket to udp_addr
     ret = kernel_bind(priv->udp_socket, priv->udp_addr_listen, sizeof(*(priv->udp_addr_listen)));
-    if(ret){
+    if (ret)
+    {
         printk(KERN_ERR "%s: Error binding UDP socket: %d\n", MODULE_NAME, ret);
         return ret;
     }
 
-    priv->udp_thread = kthread_create(ctem_packet_reception_thread,dev,"udp_thread");
-    if (IS_ERR(priv->udp_thread)){
-        printk(KERN_ERR "%s: Error creating UDP thread\n",MODULE_NAME);
+    priv->udp_thread = kthread_create(ctem_packet_reception_thread, dev, "udp_thread");
+    if (IS_ERR(priv->udp_thread))
+    {
+        printk(KERN_ERR "%s: Error creating UDP thread\n", MODULE_NAME);
         return PTR_ERR(priv->udp_thread);
     }
 
@@ -137,30 +293,34 @@ static void ctem_teardown_udp(struct net_device *dev)
     struct ctem_priv *priv;
     priv = netdev_priv(dev);
 
-    if(priv->udp_socket != NULL) 
+    if (priv->udp_socket != NULL)
         sock_release(priv->udp_socket);
-    if(priv->udp_thread != NULL)
+    if (priv->udp_thread != NULL)
         (void)kthread_stop(priv->udp_thread);
-    if(priv->udp_addr_listen != NULL)
+    if (priv->udp_addr_listen != NULL)
         kfree(priv->udp_addr_listen);
-    if(priv->udp_addr_send != NULL)
+    if (priv->udp_addr_send != NULL)
         kfree(priv->udp_addr_send);
 }
 
-void ctem_init(struct net_device *dev)
+static void ctem_init(struct net_device *dev)
 {
     struct ctem_priv *priv;
+    struct can_ml_priv *can_ml;
 
     dev->type = ARPHRD_CAN;
-	dev->mtu = CAN_MTU;
-	dev->hard_header_len = 0;
-	dev->addr_len = 0;
-	dev->tx_queue_len = 10;
+    dev->mtu = CAN_MTU;
+    dev->hard_header_len = 0;
+    dev->addr_len = 0;
+    dev->tx_queue_len = 10;
 
-	dev->flags = IFF_NOARP;
-	dev->features = NETIF_F_HW_CSUM;
+    dev->flags = IFF_NOARP;
+    dev->features = NETIF_F_HW_CSUM;
 
     dev->netdev_ops = &ctem_netdev_ops;
+    dev->ethtool_ops = &ctem_ethtool_ops;
+
+    dev->needs_free_netdev = true;
 
     /*
      * Initialize priv field
@@ -169,59 +329,46 @@ void ctem_init(struct net_device *dev)
     memset(priv, 0, sizeof(struct ctem_priv));
     priv->dev = dev;
     priv->udp_socket = NULL;
-    priv->udp_thread = NULL;   
+    priv->udp_thread = NULL;
     priv->udp_addr_listen = NULL;
-    priv->udp_addr_send = NULL; 
+    priv->udp_addr_send = NULL;
+
+    // initialize stats struct
+    priv->stats = kmalloc(sizeof(struct rtnl_link_stats64), GFP_KERNEL);
+    memset(priv->stats, 0, sizeof(struct rtnl_link_stats64));
+
+    // set can middle layer priv data
+    can_ml = (void *)priv + ALIGN(sizeof(*priv), NETDEV_ALIGN);
+    can_set_ml_priv(dev, can_ml);
+}
+
+static void ctem_setup(struct net_device *dev)
+{
+    int ret;
+
+    ctem_init(dev);
+
+    ret = ctem_setup_udp(dev, 1069, 1069);
+    if (ret)
+    {
+        printk(KERN_ERR "%s: Failed to setup udp\n", MODULE_NAME);
+    }
+
+    ctem_start_udp(dev);
 }
 
 static __exit void ctem_cleanup_module(void)
-{       
-    printk(KERN_INFO "%s: Exiting Module\n",MODULE_NAME);
+{
+    printk(KERN_DEBUG "%s: Unregistering CAN to Eth Driver\n", MODULE_NAME);
 
-    unregister_netdev(ctem_dev);    
-    ctem_teardown_udp(ctem_dev);
-    free_netdev(ctem_dev);
+    rtnl_link_unregister(&ctem_link_ops);
 }
 
 static __init int ctem_init_module(void)
 {
-    int ret = -ENOMEM;
+    printk(KERN_DEBUG "%s: Registering CAN to Eth Driver\n", MODULE_NAME);
 
-    printk(KERN_DEBUG "%s: Initalizing Module %s\n",MODULE_NAME, MODULE_NAME);   
-
-    // Allocate devices
-    ctem_dev = alloc_netdev(sizeof(struct ctem_priv), "can%d", NET_NAME_UNKNOWN, ctem_init);   
-    if (ctem_dev == NULL){
-        printk(KERN_ERR "%s: Failed to allocate device\n",MODULE_NAME);
-        goto out_alloc;
-    }
-    printk(KERN_DEBUG "%s: Allocated device.\n",MODULE_NAME);    
-
-    ret = ctem_setup_udp(ctem_dev,1069,1069);
-    if (ret)
-    {
-        printk(KERN_ERR "%s: Failed to setup udp\n", MODULE_NAME);
-        goto out_udp;
-    }
-
-    ctem_start_udp(ctem_dev);
-
-    // register devices    
-    ret = -ENODEV;
-    ret = register_netdev(ctem_dev);
-    if (ret){
-        printk(KERN_ERR "%s: error %i registering device %s\n",MODULE_NAME,ret,ctem_dev->name);
-        goto out_register;
-    }        
-    printk(KERN_DEBUG "%s: Registered device.\n",MODULE_NAME);
-
-    ret = 0;
-    goto out;
-
-    out_register: unregister_netdev(ctem_dev);
-    out_udp: ctem_teardown_udp(ctem_dev); 
-    out_alloc: free_netdev(ctem_dev);
-    out: return ret;
+    return rtnl_link_register(&ctem_link_ops);
 }
 
 module_init(ctem_init_module);
--- canToEthMod.h
+++ canToEthMod.h
@@ -2,42 +2,71 @@
 #define _CAN_TO_ETH_MOD_H
 
 #include <linux/can.h>
+#include <linux/can/core.h>
+#include <linux/can/can-ml.h>
+#include <linux/can/raw.h>
 #include <linux/errno.h>
+#include <linux/ethtool.h>
 #include <linux/if_arp.h>
+#include <linux/if_link.h>
+#include <linux/inet.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/kthread.h>
 #include <linux/module.h>
 #include <linux/netdevice.h>
+#include <linux/skbuff.h>
 #include <linux/socket.h>
+#include <net/rtnetlink.h>
 
-#define MODULE_NAME "CAN_TO_ETH_MODULE"
+#define MODULE_NAME "CanToEth"
 
-struct ctem_priv {
-    struct net_device_stats stats;
+struct ctem_priv
+{
+    struct rtnl_link_stats64 *stats;
     struct net_device *dev;
     struct socket *udp_socket;
     struct task_struct *udp_thread;
     struct sockaddr *udp_addr_listen;
     struct sockaddr *udp_addr_send;
+    int bitrate;
+};
+
+static const struct ethtool_ops ctem_ethtool_ops = {
+    .get_ts_info = ethtool_op_get_ts_info,
+};
+
+static void ctem_setup(struct net_device *dev);
+static void ctem_dellink(struct net_device *dev, struct list_head *head);
+
+static struct rtnl_link_ops ctem_link_ops __read_mostly = {
+    .kind = MODULE_NAME,
+    .priv_size = sizeof(struct ctem_priv) + sizeof(struct can_ml_priv),
+    .setup = ctem_setup,
+    .dellink = ctem_dellink,
 };
 
 static int ctem_open(struct net_device *dev);
 static int ctem_stop(struct net_device *dev);
-static netdev_tx_t ctem_xmit(struct sk_buff* skb, struct net_device *dev );
+static netdev_tx_t ctem_xmit(struct sk_buff *skb, struct net_device *dev);
+static void ctem_get_stats64(struct net_device *dev, struct rtnl_link_stats64 *storage);
+static struct net_device_stats *ctem_get_stats(struct net_device *dev);
 
 static const struct net_device_ops ctem_netdev_ops = {
     .ndo_open = ctem_open,
     .ndo_stop = ctem_stop,
     .ndo_start_xmit = ctem_xmit,
+    .ndo_get_stats64 = ctem_get_stats64,
+    .ndo_get_stats = ctem_get_stats,
 };
 
-void ctem_init(struct net_device *dev);
+static void ctem_init(struct net_device *dev);
 static void ctem_start_udp(struct net_device *dev);
 static int ctem_setup_udp(struct net_device *dev, int udp_listen_port, int udp_send_port);
 static void ctem_teardown_udp(struct net_device *dev);
 
 static int ctem_packet_reception_thread(void *arg);
+static void ctem_parse_frame(struct net_device *dev, unsigned char *buf, int size);
 
 static __exit void ctem_cleanup_module(void);
 static __init int ctem_init_module(void);
